e519342fd26e1e8c46d891cee8640c8eb3e96ad7
Pankaj Gupta <pankajbrcs@gmail.com>
2022-09-05 14:04:48 +0530

    bharatemart module 1 base application added

---
+++ src/main/java/com/bharat/emart/BharateMart/BharateMartApplication.java
@@ -0,0 +1,13 @@
+package com.bharat.emart.BharateMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharateMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharateMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bharat/emart/BharateMart/BharateMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharat.emart.BharateMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharateMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
